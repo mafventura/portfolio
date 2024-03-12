6f722589234970951d01024f61befc193e50d93e
Mafalda <mafalda.ventura@live.com.pt>
2024-03-12 13:40:56 +0000

    experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,6 +8,46 @@ export default function Experience() {
             <div class="container flex items-center mb-8">
                 <img
                     class="h-12 mr-4"
+                    src="https://seeklogo.com/images/G/general-assembly-logo-D5C634F07A-seeklogo.com.png"
+                    alt=""
+                />
+                <div class="container flex-column">
+                    <h5 class="text-bold">General Assembly</h5>
+                    <h6>Software Engineering Immersive</h6>
+                    <p>
+                        This course was held for 12 weeks and involved morning standups then
+                        proceeding with the classes which could vary from lectures, exercise labs
+                        and deliverable <a href="#projects" class="text-blue underline">projects</a>.  With this course, I was able to advance my
+                        programming knowledge and to build full-stack applications, with different
+                        stacks of languages.
+                    </p>
+                </div>
+            </div>
+            <div class="container flex items-center mb-8">
+                <img class="w-12 mr-4" src="https://i.imgur.com/9pGYfVB.png" alt="" />
+                <div class="container flex-column">
+                    <h5 class="text-bold">Quinta da Bicuda</h5>
+                    <h6>Business Operation</h6>
+                    <p>
+                        - Developed marketing materials for events
+                        <br />
+                        - Orchestrated and coordinated events
+                        <br />
+                        - Facilitated and provided support for accommodation bookings and guest
+                        stays
+                        <br />
+                        - Oversaw infrastructure management
+                        <br />
+                        - Managed supplierrelationships
+                        <br />
+                        - Handled financial management duties
+                        <br />
+                    </p>
+                </div>
+            </div>
+            <div class="container flex items-center mb-8">
+                <img
+                    class="h-12 mr-4"
                     src="https://www.iseg.ulisboa.pt/wp-content/uploads/2021/01/Simbolo-ISEG.png"
                     alt=""
                 />
@@ -17,28 +57,6 @@ export default function Experience() {
                     <p>
                         - Graduated in Management Bachelor’s degree including modules such as
                         Statistics, Marketing, Maths, Economics, Accounting, and Finance.
-                    </p>
-                </div>
-            </div>
-            <div class="container flex items-center">
-                <img class="h-12 mr-4" src="https://i.imgur.com/9pGYfVB.png" alt="" />
-                <div class="container flex-column">
-                    <h5 class="text-bold">Quinta da Bicuda</h5>
-                    <h6>Business Operation</h6>
-                    <p>
-                        - Developed marketing materials for events
-                        <br />
-                        - Orchestrated and coordinated
-                        events
-                        <br />
-                        - Facilitated and provided support for accommodation bookings and guest stays
-                        <br />
-                        - Oversaw infrastructure management
-                        <br />
-                        - Managed supplierrelationships
-                        <br />
-                        - Handled financial management duties
-                        <br />
                     </p>
                 </div>
             </div>
