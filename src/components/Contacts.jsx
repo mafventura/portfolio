0fb34f2a3bbd2185ab796bb603505635418ea2c4
Mafalda <mafalda.ventura@live.com.pt>
2024-03-24 19:34:21 +0000

    links to contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,15 +1,17 @@
 export default function Contacts() {
     return (
         <div class="container bg-white ml-5 my-12 mr-0 p-12 rounded-l-3xl shadow-lg">
-            <div class="flex">
+            <div class="flex mb-5">
                 <h3 class="text-orange">.</h3>
                 <h3>contacts</h3>
             </div>
             <div class="flex-column items-center">
                 <button>mafalda.ventura@live.com.pt</button>
                 <div class="flex ">
-                    <img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="github"/>
-                    <img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="linkedin"/>
+                {/* eslint-disable-next-line */}
+                    <a href="https://github.com/mafventura" target="_blank"><img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="github"/></a>
+                {/* eslint-disable-next-line */}
+                    <a href="https://www.linkedin.com/in/mafaldacventura/" target="_blank"><img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="linkedin"/></a>
                 </div>
             </div>
         </div>
