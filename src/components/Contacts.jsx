8f8ee6ffe892ca20419c3f84b56524006f0bd82a
Mafalda <mafalda.ventura@live.com.pt>
2024-03-24 19:53:11 +0000

    contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -5,14 +5,31 @@ export default function Contacts() {
                 <h3 class="text-orange">.</h3>
                 <h3>contacts</h3>
             </div>
+
             <div class="flex-column items-center">
-                <button>mafalda.ventura@live.com.pt</button>
-                <div class="flex ">
                 {/* eslint-disable-next-line */}
-                    <a href="https://github.com/mafventura" target="_blank"><img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="github"/></a>
+                <a href="mailto:mafalda.ventura@live.com.pt" target="_blank" class="flex items-center mb-4 hover:text-orange active:text-blue">
+                    <img class="h-9 me-2" src="https://i.imgur.com/ItE2Fxo.png" alt="mail" />
+                    mafalda.ventura@live.com.pt
+                </a>
                 {/* eslint-disable-next-line */}
-                    <a href="https://www.linkedin.com/in/mafaldacventura/" target="_blank"><img class="h-9" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" alt="linkedin"/></a>
-                </div>
+                <a href="https://github.com/mafventura" target="_blank" class="flex items-center mb-4 hover:text-orange active:text-blue">
+                    <img
+                        class="h-9 me-2"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg"
+                        alt="github"
+                    />{" "}
+                    /in/mafaldacventura/
+                </a>
+                {/* eslint-disable-next-line */}
+                <a href="https://www.linkedin.com/in/mafaldacventura/" target="_blank" class="flex items-center mb-4 hover:text-orange active:text-blue">
+                    <img
+                        class="h-9 me-2"
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg"
+                        alt="linkedin"
+                    />
+                    github.com/mafventura
+                </a>
             </div>
         </div>
     );
